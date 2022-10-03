6ffcd0fd19ceb6a50690ceda94957ebaccf5922f
kimsijun <papaya9349@naver.com>
2022-10-03 14:15:10 +0900

    백준 15649

--- src/Baekjoon1260.java
+++ src/Baekjoon1260.java
@@ -1,128 +1,128 @@
-import java.util.Scanner;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+// import java.util.Scanner;
+// import java.util.LinkedList;
+// import java.util.Queue;
+// import java.util.Stack;
 
-class Node {
-  int data = 0;
-  boolean mark = false;
-  LinkedList<Node> adjacent = new LinkedList<>();
-}
+// class Node {
+// int data = 0;
+// boolean mark = false;
+// LinkedList<Node> adjacent = new LinkedList<>();
+// }
 
-class Main {
-  Node[] nodes;
+// class Main {
+// Node[] nodes;
 
-  public static void main(String[] args) {
-    new Main().solution();
-  }
+// public static void main(String[] args) {
+// new Main().solution();
+// }
 
-  void solution() {
-    Scanner scanner = new Scanner(System.in);
-    int N = scanner.nextInt(); // 정점 개수
-    int M = scanner.nextInt(); // 간선 개수
-    int V = scanner.nextInt(); // 시작 정점
+// void solution() {
+// Scanner scanner = new Scanner(System.in);
+// int N = scanner.nextInt(); // 정점 개수
+// int M = scanner.nextInt(); // 간선 개수
+// int V = scanner.nextInt(); // 시작 정점
 
-    // 노드 설정
-    nodes = new Node[N];
-    for (int i = 1; i <= N; i++) {
-      nodes[i - 1] = new Node();
-      nodes[i - 1].data = i;
-    }
+// // 노드 설정
+// nodes = new Node[N];
+// for (int i = 1; i <= N; i++) {
+// nodes[i - 1] = new Node();
+// nodes[i - 1].data = i;
+// }
 
-    // 간선 설정
-    for (int i = 0; i < M; i++) {
-      int a = scanner.nextInt();
-      int b = scanner.nextInt();
-      if (a > b) {
-        setEdge(a, b);
-      } else {
-        setEdge(b, a);
-      }
-    }
+// // 간선 설정
+// for (int i = 0; i < M; i++) {
+// int a = scanner.nextInt();
+// int b = scanner.nextInt();
+// if (a > b) {
+// setEdge(a, b);
+// } else {
+// setEdge(b, a);
+// }
+// }
 
-    adjacentSort();
+// adjacentSort();
 
-    dfsR(V);
-    System.out.println();
-    initNodes();
-    bfs(V);
-    System.out.println();
+// dfsR(V);
+// System.out.println();
+// initNodes();
+// bfs(V);
+// System.out.println();
 
-    scanner.close();
-  }
+// scanner.close();
+// }
 
-  void initNodes() {
-    for (Node node : nodes) {
-      node.mark = false;
-    }
-  }
+// void initNodes() {
+// for (Node node : nodes) {
+// node.mark = false;
+// }
+// }
 
-  void adjacentSort() {
-    for (Node node : nodes) {
-      node.adjacent.sort((o1, o2) -> o1.data - o2.data);
-    }
-  }
+// void adjacentSort() {
+// for (Node node : nodes) {
+// node.adjacent.sort((o1, o2) -> o1.data - o2.data);
+// }
+// }
 
-  void setEdge(int a, int b) {
-    Node node1 = nodes[a - 1];
-    Node node2 = nodes[b - 1];
-    if (!node1.adjacent.contains(node2)) {
-      node1.adjacent.add(node2);
-    }
-    if (!node2.adjacent.contains(node1)) {
-      node2.adjacent.add(node1);
-    }
-  }
+// void setEdge(int a, int b) {
+// Node node1 = nodes[a - 1];
+// Node node2 = nodes[b - 1];
+// if (!node1.adjacent.contains(node2)) {
+// node1.adjacent.add(node2);
+// }
+// if (!node2.adjacent.contains(node1)) {
+// node2.adjacent.add(node1);
+// }
+// }
 
-  void printNode(Node node) {
-    System.out.print(node.data + " ");
-  }
+// void printNode(Node node) {
+// System.out.print(node.data + " ");
+// }
 
-  void dfs(int v) {
-    Stack<Node> stack = new Stack<>();
-    stack.push(nodes[v - 1]);
+// void dfs(int v) {
+// Stack<Node> stack = new Stack<>();
+// stack.push(nodes[v - 1]);
 
-    while (!stack.isEmpty()) {
-      Node node = stack.pop();
-      for (Node n : node.adjacent) {
-        if (n.mark == false) {
-          stack.push(n);
-        }
-        break;
-      }
-      node.mark = true;
-      printNode(node);
-    }
-  }
+// while (!stack.isEmpty()) {
+// Node node = stack.pop();
+// for (Node n : node.adjacent) {
+// if (n.mark == false) {
+// stack.push(n);
+// }
+// break;
+// }
+// node.mark = true;
+// printNode(node);
+// }
+// }
 
-  void dfsR(int v) {
-    if (nodes[v - 1] == null) {
-      return;
-    } else {
-      Node node = nodes[v - 1];
-      node.mark = true;
-      printNode(node);
-      for (Node n : node.adjacent) {
-        if (n.mark == false) {
-          dfsR(n.data);
-        }
-      }
-    }
-  }
+// void dfsR(int v) {
+// if (nodes[v - 1] == null) {
+// return;
+// } else {
+// Node node = nodes[v - 1];
+// node.mark = true;
+// printNode(node);
+// for (Node n : node.adjacent) {
+// if (n.mark == false) {
+// dfsR(n.data);
+// }
+// }
+// }
+// }
 
-  void bfs(int v) {
-    Queue<Node> queue = new LinkedList<>();
-    queue.add(nodes[v - 1]);
-    while (!queue.isEmpty()) {
-      Node node = queue.poll();
-      node.mark = true;
-      printNode(node);
-      for (Node n : node.adjacent) {
-        if (n.mark == false) {
-          n.mark = true;
-          queue.add(n);
-        }
-      }
-    }
-  }
-}
+// void bfs(int v) {
+// Queue<Node> queue = new LinkedList<>();
+// queue.add(nodes[v - 1]);
+// while (!queue.isEmpty()) {
+// Node node = queue.poll();
+// node.mark = true;
+// printNode(node);
+// for (Node n : node.adjacent) {
+// if (n.mark == false) {
+// n.mark = true;
+// queue.add(n);
+// }
+// }
+// }
+// }
+// }
--- src/Baekjoon15649.java
+++ src/Baekjoon15649.java
@@ -1,5 +1,48 @@
-class Main {
-  public static void main(String[] args) {
+import java.util.Scanner;
 
+class Main {
+  static int N, M;
+  static int[] arr;
+  static boolean[] isUsed;
+
+  public static void main(String[] args) {
+    new Main().solution();
+  }
+
+  void solution() {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+    M = sc.nextInt();
+
+    isUsed = new boolean[N + 1];
+    arr = new int[M + 1];
+
+    sc.close();
+    System.out.println("---");
+
+    backstracking(0);
+  }
+
+  void backstracking(int idx) {
+    if (idx == M) {
+      printResult();
+      return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+      if (!isUsed[i]) {
+        isUsed[i] = true;
+        arr[idx] = i;
+        backstracking(idx + 1); // arr의 인덱스를 +1 시켜준다.
+        isUsed[i] = false;
+      }
+    }
+  }
+
+  void printResult() {
+    for (int i = 0; i < M; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
   }
 }
